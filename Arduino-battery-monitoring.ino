d157328dfd86699f86ee0f6f1ad25dc3aa2589c6
carlocostacc <49722518+carlocostacc@users.noreply.github.com>
2023-07-23 01:05:21 -0400

    Update Arduino-battery-monitoring.ino
    new code

--- Arduino-battery-monitoring.ino
+++ Arduino-battery-monitoring.ino
@@ -1,23 +1,49 @@
+/*
+  Battery Monitor
 
+  This example creates a Bluetooth® Low Energy peripheral with the standard battery service and
+  level characteristic. The A0 pin is used to calculate the battery level.
+
+  The circuit:
+  - Arduino MKR WiFi 1010, Arduino Uno WiFi Rev2 board, Arduino Nano 33 IoT,
+    Arduino Nano 33 BLE, or Arduino Nano 33 BLE Sense board.
+
+  You can use a generic Bluetooth® Low Energy central app, like LightBlue (iOS and Android) or
+  nRF Connect (Android), to interact with the services and characteristics
+  created in this sketch.
+
+  This example code is in the public domain.
+*/
+#include <Wire.h>
+#include "MAX30105.h"
+
+#include "heartRate.h"
+
+MAX30105 particleSensor;
 #include <ArduinoBLE.h>
 
- // Bluetooth® Low Energy Battery Service
-BLEService batteryService("180F");
+ // Bluetooth® Heart Rate Service
+BLEService Heart_rate("180D");
 
 // Bluetooth® Low Energy Battery Level Characteristic
-BLEUnsignedCharCharacteristic batteryLevelChar("2A19",  // standard 16-bit characteristic UUID
+BLEUnsignedCharCharacteristic heart_rateCHAR("2A37",  // standard 16-bit characteristic UUID
     BLERead | BLENotify); // remote clients will be able to get notifications if this characteristic changes
 
-BLEService customService( ); // Custom service UUID
-
-BLECharacteristic customCharacteristic("0000dfb1-0000-1000-8000-00805f9b34fb", // Custom characteristic UUID
-                                       BLEWrite | BLERead | BLENotify, 20); // Properties and maximum size
-
-int oldBatteryLevel = 0;  // last battery level reading from analog input
+int oldbattery = 0;  // last battery level reading from analog input
 long previousMillis = 0;  // last time the battery level was checked, in ms
 
+
+//variables for the heart rate sensor
+const byte RATE_SIZE = 4; //Increase this for more averaging. 4 is good.
+byte rates[RATE_SIZE]; //Array of heart rates
+byte rateSpot = 0;
+long lastBeat = 0; //Time at which the last beat occurred
+
+float beatsPerMinute;
+int beatAvg;
+
 void setup() {
-  Serial.begin(9600);    // initialize serial communication
+  Serial.begin(115200);    // initialize serial communication
   while (!Serial);
 
   pinMode(LED_BUILTIN, OUTPUT); // initialize the built-in LED pin to indicate when a central is connected
@@ -35,15 +61,10 @@ void setup() {
      The name can be changed but maybe be truncated based on space left in advertisement packet
   */
   BLE.setLocalName("CarloMonitoring");
-  String address = BLE.address();
-
-  Serial.print("Local address is: ");
-  Serial.println(address);
-
-  BLE.setAdvertisedService(batteryService); // add the service UUID
-  batteryService.addCharacteristic(batteryLevelChar); // add the battery level characteristic
-  BLE.addService(batteryService); // Add the battery service
-  batteryLevelChar.writeValue(oldBatteryLevel); // set initial value for this characteristic
+  BLE.setAdvertisedService(Heart_rate); // add the service UUID
+  Heart_rate.addCharacteristic(heart_rateCHAR); // add the battery level characteristic
+  BLE.addService(Heart_rate); // Add the battery service
+  heart_rateCHAR.writeValue(oldbattery); // set initial value for this characteristic
 
   /* Start advertising Bluetooth® Low Energy.  It will start continuously transmitting Bluetooth® Low Energy
      advertising packets and will be visible to remote Bluetooth® Low Energy central devices
@@ -53,6 +74,23 @@ void setup() {
   BLE.advertise();
 
   Serial.println("Bluetooth® device active, waiting for connections...");
+
+
+
+//  setup for the heart rate sensor
+  Serial.println("Initializing heart rate sensor...");
+
+  // Initialize sensor
+  if (!particleSensor.begin(Wire, 400000)) //Use default I2C port, 400kHz speed
+  {
+    Serial.println("MAX30102 was not found. Please check wiring/power. ");
+    while (1);
+  }
+  Serial.println("Place your index finger on the sensor with steady pressure.");
+
+  particleSensor.setup(); //Configure sensor with default settings
+  particleSensor.setPulseAmplitudeRed(0x0A); //Turn Red LED to low to indicate sensor is running
+  particleSensor.setPulseAmplitudeGreen(0); //Turn off Green LED
 }
 
 void loop() {
@@ -74,7 +112,8 @@ void loop() {
       // if 200ms have passed, check the battery level:
       if (currentMillis - previousMillis >= 200) {
         previousMillis = currentMillis;
-        updateBatteryLevel();
+        Getheartrate();
+        updateBatteryLevel(((particleSensor.getIR()/10) -20));
       }
     }
     // when the central disconnects, turn off the LED:
@@ -84,17 +123,52 @@ void loop() {
   }
 }
 
-void updateBatteryLevel() {
+void updateBatteryLevel(int heart_rate) {
   /* Read the current voltage level on the A0 analog input pin.
      This is used here to simulate the charge level of a battery.
   */
-  int battery = analogRead(A0);
-  int batteryLevel = map(battery, 0, 1023, 0, 100);
+  int battery = heart_rate;
+  Serial.println(battery);
+  heart_rateCHAR.writeValue(battery);
+  
+}
 
-  if (batteryLevel != oldBatteryLevel) {      // if the battery level has changed
-    Serial.print("Battery Level % is now: "); // print it
-    Serial.println(batteryLevel);
-    batteryLevelChar.writeValue(batteryLevel);  // and update the battery level characteristic
-    oldBatteryLevel = batteryLevel;           // save the level for next comparison
+
+int Getheartrate(){
+  long irValue = particleSensor.getIR();
+
+  if (checkForBeat(irValue) == true)
+  {
+    //We sensed a beat!
+    long delta = millis() - lastBeat;
+    lastBeat = millis();
+
+    beatsPerMinute = 60 / (delta / 1000.0);
+
+    if (beatsPerMinute < 255 && beatsPerMinute > 20)
+    {
+      rates[rateSpot++] = (byte)beatsPerMinute; //Store this reading in the array
+      rateSpot %= RATE_SIZE; //Wrap variable
+
+      //Take average of readings
+      beatAvg = 0;
+      for (byte x = 0 ; x < RATE_SIZE ; x++)
+        beatAvg += rates[x];
+      beatAvg /= RATE_SIZE;
+    }
   }
+
+  
+  Serial.print("IR=");
+  Serial.print(irValue);
+  Serial.print(", BPM=");
+  Serial.print(beatsPerMinute);
+  Serial.print(", Avg BPM=");
+  Serial.print(beatAvg);
+
+  if (irValue < 50000)
+    Serial.print(" No finger?");
+
+  Serial.println();
+  return beatsPerMinute;
 }
